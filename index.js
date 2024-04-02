def5785e589b6c5b78cb2d02e2242068675674b5
mohit <mohitmakhijani123@gmail.com>
2024-04-03 02:15:58 +0530

    node-9

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const server = express();
 const model = require("./model/product");
 const cors = require('cors'); 
 const Product = model.Product;
+const path = require('path'); // Import the path module
+
 server.use(express.json());
 server.use(cors());
+
+// Serve static files from the "public" directory
+server.use(express.static(path.join(__dirname, 'public')));
+
 server.post("/", async (req, res) => {
     try {
         const product = new Product(req.body);
@@ -17,22 +23,18 @@ server.post("/", async (req, res) => {
     }
 });
 
-server.get('/',async (req,res)=>{
+server.get('/', async (req, res) => {
     const products = await Product.find();
-   res.status(200).json({ products });
-})
-
-
-main().catch((error) => console.log(error));
-
-
+    res.status(200).json({ products });
+});
 
 async function main() {
   await mongoose.connect("mongodb+srv://mohitmakhijani7:1234123121@cluster0.e2soqtd.mongodb.net/products");
   console.log("database connected");
 }
 
-server.listen(8080, ()=> {
-    console.log('Server running ');
-  });
-  
+main().catch((error) => console.log(error));
+
+server.listen(8080, () => {
+    console.log('Server running on http://localhost:8080');
+});
